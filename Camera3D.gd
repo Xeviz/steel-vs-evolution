ed4b5f626a8a8c2e3941b7204c2a9f3f6ad91aeb
Xeviz <kubas2467@gmail.com>
2024-07-01 08:45:39 +0200

    first commit

---
+++ Camera3D.gd
@@ -0,0 +1,15 @@
+extends Camera3D
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_parent().get_node("Player")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player != null:
+		position.x = player.position.x
+		position.z = player.position.z
+	pass
