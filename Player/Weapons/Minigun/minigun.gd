7f2ec42646f919f7c37e3c8b6d2d101a78ef9b09
Xeviz <kubas2467@gmail.com>
2024-10-30 08:30:43 +0100

    basic minigun animation added

--- Player/Weapons/Minigun/minigun.gd
+++ Player/Weapons/Minigun/minigun.gd
@@ -1,6 +1,8 @@
 extends Weapon
 class_name Minigun
 
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = $AnimationTree.get("parameters/playback")
 @export var bullet_scene: PackedScene
 var base_weapon_cost = 1
 
--- States/Weapons/Minigun/minigun_firing.gd
+++ States/Weapons/Minigun/minigun_firing.gd
@@ -4,6 +4,9 @@ class_name MinigunFiring
 @export var minigun : Minigun
 var fire_timer = 0.0
 
+func enter():
+	minigun.animation_state.travel("MinigunShooting")
+	print("zaczynam strzelac")
 
 func update(delta: float):
 	fire_timer -= delta
@@ -13,3 +16,6 @@ func update(delta: float):
 		minigun.ammo_left -= 1
 	if minigun.ammo_left <= 0:
 		state_transition.emit(self, "WeaponReloading")
+		
+func exit():
+	minigun.animation_state.travel("MinigunIdle")
